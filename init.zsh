5ebeb5ef7293aaf746ab701f129cf88104f15d65
Bao Hengtao <baohengtao@gmail.com>
2021-04-17 10:35:00 +0800

    Use zinit instead of antogen

--- init.zsh
+++ init.zsh
@@ -1,7 +1,6 @@
 export ZSH_DIR="${0%/*}"  # $0 当前文件； ${var%pattern} 将移除匹配的pattern
 
 files=(
-  env_private.zsh 
   env.zsh
   plugin.zsh
   plugin_sys.zsh
--- plugin.zsh
+++ plugin.zsh
@@ -1,35 +1,47 @@
 export ZSH_Plug=$HOME/.zsh_plug  # plugin 存储位置
-export OMZ=$ZSH_Plug/.oh-my-zsh
-export ATG=$ZSH_Plug/.antigen.zsh
+
 
 
 # load oh-my-zsh plugins
-plugins=(
-  history #suggestions
-  zsh-interactive-cd   #navigation
-  cp copyfile copydir copybuffer   #copy
-  mosh docker pip #application
-  git docker-compose vscode  #alias   
-  colored-man-pages man   fzf
-  )
+
+load_omz(){
+  if ! [[ -e $ZSH_Plug/.oh-my-zsh ]]; then
+    git clone  https://github.com/ohmyzsh/ohmyzsh.git $ZSH_Plug/.oh-my-zsh
+  fi
+
+  plugins=(
+    history #suggestions
+    zsh-interactive-cd   #navigation
+    cp copyfile copydir copybuffer   #copy
+    mosh docker pip #application
+    git docker-compose vscode  #alias   
+    colored-man-pages man   fzf
+    )
+  source $ZSH_Plug/.oh-my-zsh/oh-my-zsh.sh
+}
 
 
-
-if ! [[ -e $OMZ ]]; then
-   git clone --depth 1 https://github.com/ohmyzsh/ohmyzsh.git $OMZ
-fi
-source $OMZ/oh-my-zsh.sh
-
-
-
-
-[[ -e $ATG ]] || curl -L git.io/antigen > $ATG
-source $ATG
+load_zinit(){
+  if [[ ! -f $ZSH_Plug/.zinit/bin/zinit.zsh ]]; then
+      print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+      command mkdir -p "$ZSH_Plug/.zinit" && command chmod g-rwX "$ZSH_Plug/.zinit"
+      command git clone https://github.com/zdharma/zinit "$ZSH_Plug/.zinit/bin" && \
+          print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
+          print -P "%F{160}▓▒░ The clone has failed.%f%b"
+  fi
 
 
-antigen bundle zdharma/fast-syntax-highlighting 
-antigen bundle zsh-users/zsh-autosuggestions  
-antigen bundle zsh-users/zsh-history-substring-search 
-antigen theme denysdovhan/spaceship-prompt
-antigen apply
+  source "$ZSH_Plug/.zinit/bin/zinit.zsh"
+  zinit load zdharma/history-search-multi-word
+  zinit light zdharma/fast-syntax-highlighting 
+  zinit load  zsh-users/zsh-history-substring-search 
+  zinit load  zsh-users/zsh-autosuggestions  
+  zinit light denysdovhan/spaceship-prompt
 
+
+  autoload -Uz _zinit
+  (( ${+_comps} )) && _comps[zinit]=_zinit
+}
+
+load_omz
+load_zinit
