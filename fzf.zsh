0db9a6c8e40f06cc8a206a1d1ae01ec6d15e0ab4
Bao Hengtao <baohengtao@gmail.com>
2021-04-24 12:35:25 +0800

    Modify  fzf.zsh

--- fzf.zsh
+++ fzf.zsh
@@ -1,15 +1,16 @@
 # 使用 `;` 触发搜索
 export FZF_COMPLETION_TRIGGER=';'
 # 设置UI选项
-export FZF_DEFAULT_OPTS='--height 80% --layout=reverse --border --multi --info=inline --exact  '
-export FZF_ALT_C_OPTS=''
-export FZF_CTRL_T_OPTS=''
-export FZF_CTRL_R_OPTS=''
-export FORGIT_FZF_DEFAULT_OPTS="--cycle --reverse" 
+export FZF_DEFAULT_OPTS='--height 80% --layout=reverse --border --multi --info=inline --exact'
+
+export FZF_ALT_C_OPTS="$FZF_DEFAULT_OPTS --preview 'tree -C {} | head -200'"
+export FZF_CTRL_T_OPTS="$FZF_DEFAULT_OPTS --preview '(highlight -O ansi {} || cat {} || tree -C {} | head -200) 2> /dev/null' "
+#export FZF_CTRL_R_OPTS=''
+#export FORGIT_FZF_DEFAULT_OPTS="--cycle --reverse" 
 # 设置执行命令
-export FZF_DEFAULT_COMMAND="fd -tf --no-ignore-vcs "
+export FZF_DEFAULT_COMMAND="fd  --no-ignore-vcs "
 export FZF_ALT_C_COMMAND="fd -td --no-ignore-vcs  . ~"
-export FZF_CTRL_T_COMMAND="fd -tf --no-ignore-vcs "
+export FZF_CTRL_T_COMMAND="fd  --no-ignore-vcs "
 # 设置路径生产使用的函数
 _fzf_compgen_path() { fd --no-ignore-vcs   . "$1"}
 _fzf_compgen_dir() { fd -td --no-ignore-vcs   . "$1"}
