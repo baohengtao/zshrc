7a1e5e1033b8d8917a235871f4d2d972048bb73d
Bao Hengtao <baohengtao@users.noreply.github.com>
2021-05-29 15:21:07 +0800

    regular update

--- parameter.zsh
+++ parameter.zsh
@@ -2,12 +2,6 @@
 autoload -Uz compinit # init completion system
 compinit -d $XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION 
 
-# ## XDG
-# export XDG_CONFIG_HOME="$HOME/config"
-# export XDG_CACHE_HOME="$HOME/.cache"
-# export XDG_DATA_HOME="$HOME/.local/share"
-
-
 ## history
 HISTFILE=$XDG_DATA_HOME/.zsh_history
 HISTSIZE=10000
