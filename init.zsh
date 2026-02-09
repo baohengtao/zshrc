f7f51cbe2175c84bbcf152e0c37d5fb650e6ae92
Bao Hengtao <baohengtao@users.noreply.github.com>
2026-02-09 19:43:28 +0800

    change zsh_history dir

--- init.zsh
+++ init.zsh
@@ -1,5 +1,6 @@
 autoload -Uz compinit # init completion system
-compinit -d $XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION
+[[ -d "$ZDOTDIR/data" ]] || mkdir -p "$ZDOTDIR/data"
+compinit -d $ZDOTDIR/data/zcompdump-$ZSH_VERSION
 
 
 current="${0%/*}"
@@ -9,7 +10,7 @@ source "$current/exiftool.zsh"
 source "$current/fzf.zsh"
 
 ## history
-export HISTFILE=$XDG_DATA_HOME/.zsh_history
+export HISTFILE=$ZDOTDIR/data/zsh_history
 export HISTSIZE=1000000
 export SAVEHIST=1000000
 setopt appendhistory
